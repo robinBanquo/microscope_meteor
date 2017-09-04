f680d6f86793aa943b8d7ad6f78d04ec48e0511a
robinBanquo <caramaschi@hotmail.com>
2017-09-04 16:13:52 +0200

    formulaire d'edit des post + erreurs

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,16 +7,32 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
+
         Meteor.call('postInsert', post, function(error, result) {
             // affiche l'erreur à l'utilisateur et s'interrompt
             if (error){
-                return alert(error.reason);
+                return throwError(error.reason);
             }
             // affiche ce résultat mais route tout de même
             if (result.postExists){
-                alert('Ce lien a déjà été utilisé');
+                throwError('Ce lien a déjà été utilisé');
             }
             Router.go('postPage', {_id: result._id});
         });
+    }
+});
+
+Template.postSubmit.onCreated(function() {
+    Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
     }
 });
